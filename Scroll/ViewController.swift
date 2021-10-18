f64253c144dad3b2b1faa48de180bc804e4c063f
sandy <sandyambarita@gmail.com>
2021-10-18 14:13:32 +0700

    Initial Commit

---
+++ Scroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scroll
+//
+//  Created by Sandy Ambarita on 18/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
