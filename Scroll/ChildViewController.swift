a9bfcb21dc6b76b7d9729038fb35617ad49202ec
sandy <sandyambarita@gmail.com>
2021-10-18 17:50:19 +0700

    first commit

---
+++ Scroll/ChildViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ChildViewController.swift
+//  Scroll
+//
+//  Created by Sandy Ambarita on 18/10/21.
+//
+
+import Foundation
---
+++ Scroll/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewControllere.swift
+//  Scroll
+//
+//  Created by Sandy Ambarita on 18/10/21.
+//
+
+import Foundation
